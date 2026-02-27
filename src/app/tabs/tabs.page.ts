637db60103092f393d7833bb02c0dad0c6ea8bbe
bailin wei <weibailin@hotmail.com>
2026-02-28 07:12:51 +0800

    add tabs animation

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  @ViewChild('tabs', { static: false }) tabs!: IonTabs;
+  selectedTab: any = 'tab1';
 
   constructor() {}
 
+  setCurrentTab() {
+    this.selectedTab = this.tabs.getSelected();
+  }
 }
